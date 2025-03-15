eb1831338ef201053265c2b4b242b761d27b7253
satyalyadav <satyalyadav10@gmail.com>
2025-03-15 00:18:04 -0700

    Layout, Nav, Sidebar

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,10 +2,11 @@
 
 import { ID, Query } from "node-appwrite";
 import { appwriteConfig } from "../appwrite/config";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { pasreStringify } from "../utils";
 import { cookies } from "next/headers";
 import path from "path";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
     const { databases } = await createAdminClient();
@@ -58,7 +59,7 @@ export const createAccount = async ({
             {
                 fullName,
                 email,
-                avatar: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+                avatar: avatarPlaceholderUrl,
                 accountId,
             },
         );
@@ -92,4 +93,19 @@ export const verifySecret = async ({
     } catch (error) {
         handleError(error, "Failed to verify secret");
     }
+}
+
+export const getCurrentUser = async () => {
+    const { databases, account } = await createSessionClient();
+    const result = await account.get();
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        [Query.equal("accountId", result.$id)],
+    );
+
+    if(user.total <= 0) {
+        return null;
+    }
+    return pasreStringify(user.documents[0]);
 }
